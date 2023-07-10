0887389b3cb21fc486f5dc4802175104cabf4819
Prachi Patil <124233543+PatilPrachi1411@users.noreply.github.com>
2023-07-10 16:47:28 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/car-price-prediction")
+def carpriceprediction():
+    return render_template("carpriceprediction.html")
+
+@app.route("/car-price-prediction-result")
+def carpricepredictionresult():
+    return render_template("carpricepredictionresult.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
